7f1c4531b516b6b74f91d9a003cf67699b86cc3f
Michael-Hernandezz <160257430+Michael-Hernandezz@users.noreply.github.com>
2025-09-28 17:38:40 -0500

    creacion proyecto EstudianteService

--- src/test/java/co/edu/uceva/estudianteservice/EstudianteServiceApplicationTests.java
+++ src/test/java/co/edu/uceva/estudianteservice/EstudianteServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EstudianteServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
