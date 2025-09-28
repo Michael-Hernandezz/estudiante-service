a034923f4a87d5a53c34138d3772367bb45fd49f
Michael-Hernandezz <160257430+Michael-Hernandezz@users.noreply.github.com>
2025-09-28 17:47:34 -0500

    Spring Data JPA

---
+++ src/main/java/co/edu/uceva/estudianteservice/model/repositories/IEstudianteRepository.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.estudianteservice.model.repositories;
+
+import co.edu.uceva.estudianteservice.model.entities.Estudiante;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Interface que hereda de CrudRepository para realizar las
+ * operaciones de CRUD sobre la entidad Producto
+ */
+//Debe especificarse la entidad y el tipo de Dato primario, es decir, Long por Id
+public interface IEstudianteRepository extends CrudRepository<Estudiante, Long> {
+
+}
