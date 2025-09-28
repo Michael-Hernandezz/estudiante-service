3b341b191028472b758cf69105ae311bcbfbc56d
Michael-Hernandezz <160257430+Michael-Hernandezz@users.noreply.github.com>
2025-09-28 17:53:01 -0500

    Teoría: Capa de Servicio

---
+++ src/main/java/co/edu/uceva/estudianteservice/model/services/IEstudianteService.java
@@ -0,0 +1,19 @@
+package co.edu.uceva.estudianteservice.model.services;
+
+import co.edu.uceva.estudianteservice.model.entities.Estudiante;
+import java.util.List;
+
+/**
+ * Interface que define los métodos que se pueden realizar sobre la entidad Producto
+ */
+
+
+public interface IEstudianteService {
+    Estudiante save(Estudiante estudiante);
+    void delete(Estudiante estudiante);
+    Estudiante findById(Long id);
+    Estudiante findByApellido(String apellido);
+    Estudiante findByEmail(String email);
+    Estudiante update(Estudiante estudiante);
+    List<Estudiante> findAll();
+}
